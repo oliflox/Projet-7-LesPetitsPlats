3db45e96d328d7eeb75bf207ef35554130fbf3e3
oliflox <83953551+oliflox@users.noreply.github.com>
2025-03-19 21:19:45 +0100

    Add logo image to SearchContainer component

--- js/components/SearchContainer.js
+++ js/components/SearchContainer.js
@@ -8,6 +8,7 @@ export const SearchContainer = () => {
     return `
     <section class="searchSection">
         <header class="headerLogo">
+            <img src="./assets/img/Logo.png" alt="logo">
         </header>
         <h1 class="headerTitle">cherchez parmi plus de 1500 recettes du quotidien, simples et délicieuses</h1>
         <div class="searchInputContainer">
